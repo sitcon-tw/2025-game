4e1939776cc41c0efb8b9040749772f5e0a4eb97
星月なるこ <projectcsj@gmail.com>
2025-01-21 23:36:45 +0800

    feat: add type definitions file

---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+/* ? This is a type file
+? You should define all your types here
+? Here have an example of how to define a type
+? export type typename = { first_var: first_var_type; second_var: second_var_type; };
+? You can import type to anywhere like this
+? import { typename } from "@/types";
+ */
