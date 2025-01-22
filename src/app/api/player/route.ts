75137e3a44ffcad0c7b625f91347f15ad8cd3503
星月なるこ <projectcsj@gmail.com>
2025-01-22 22:58:10 +0800

    feat: add player API routes for GET  requests (POST W.I.P.)

---
+++ src/app/api/player/route.ts
@@ -0,0 +1,35 @@
+import { createPlayer, getPlayer } from "@/utils/query";
+import { notFound, success } from "@/utils/response";
+import { NextRequest } from "next/server";
+
+export /**
+ * @description
+ * @author 星月なるこ
+ * @date 2025-01-22 22:54:21
+ * @param {NextRequest} request
+ * @return {success | notFound}
+ */
+const GET = async (request: NextRequest) => {
+  /** @type {string | undefined} */
+  const playerId: string | undefined = request.nextUrl.searchParams
+    .get("playerId")
+    ?.toString();
+  /** @type {object | undefined} */
+  const player: object | undefined = await getPlayer(String(playerId));
+  if (!player) return notFound("Player not found");
+  return success(player);
+};
+
+export /**
+ * @description
+ * @author 星月なるこ
+ * @date 2025-01-22 22:55:00
+ * @param {NextRequest} request
+ * @return {*}
+ */
+const POST = async (request: NextRequest) => {
+  const playerData = await request.json();
+  const player = await createPlayer(playerData);
+  if (!player) return 500;
+  return success({ message: "User regist complete" });
+};
