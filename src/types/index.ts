5b47fca3a1825df99852f0170bdaf1a475ae0226
星月なるこ <projectcsj@gmail.com>
2025-01-22 22:36:11 +0800

    feat: add PlayerData type definition for player information

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,5 +3,10 @@
 ? Here have an example of how to define a type
 ? export type typename = { first_var: first_var_type; second_var: second_var_type; };
 ? You can import type to anywhere like this
-? import { typename } from "@/types";
+? import { typename, playerData } from '@/types';
  */
+
+export type PlayerData = {
+  name: string;
+  linktree: string;
+};
