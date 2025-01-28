f8458b25efd72ed5228d2fb2550ab5d6b2f6fc05
Dada878 <xx0932399@gmail.com>
2025-01-28 22:57:12 +0800

    feat: enhance layout with footer navigation and responsive design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import texts from "@/data/metadata.json";
+"use client";
 
-export const metadata: Metadata = {
-  title: texts.title,
-  description: texts.description,
-};
+import "./globals.css";
+import Link from "next/link";
+import { Blocks, Gamepad, LucideIcon, Store, UserRound } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
@@ -14,7 +13,43 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-Hant-TW">
-      <body className="m-4 h-screen">{children}</body>
+      <body className="flex h-screen flex-col overflow-hidden">
+        <div className="h-full w-full">{children}</div>
+        <div className="w-full">
+          <footer className="flex border-t-2 border-gray-400 bg-gray-200 px-2 py-2 text-center text-sm text-gray-600">
+            <NavbarItem href="/" name="遊戲" Icon={Gamepad} />
+            <NavbarItem href="/fragment" name="板塊" Icon={Blocks} />
+            <NavbarItem href="/store" name="商店" Icon={Store} />
+            <NavbarItem href="/personal" name="個人" Icon={UserRound} />
+          </footer>
+        </div>
+      </body>
     </html>
+  );
+}
+
+function NavbarItem({
+  href,
+  name,
+  Icon,
+}: Readonly<{
+  href: string;
+  name: string;
+  Icon: LucideIcon;
+}>) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+  return (
+    <Link href={href} className="flex-1 px-4">
+      <div
+        className={cn(
+          "flex flex-col items-center justify-center gap-1 px-3 py-1",
+          isActive && "border-b-2 border-blue-500 text-blue-500",
+        )}
+      >
+        <Icon strokeWidth={2} size={32} />
+        {name}
+      </div>
+    </Link>
   );
 }
