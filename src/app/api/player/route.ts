07531a3ba13d11f938dd4c514d1864ad23e3eb0c
星月なるこ <projectcsj@gmail.com>
2025-02-05 20:35:49 +0800

    feat: export GET and POST functions in player route for improved API accessibility

--- src/app/api/player/route.ts
+++ src/app/api/player/route.ts
@@ -2,14 +2,14 @@ import { createPlayer, getPlayer } from "@/utils/query";
 import { internalServerError, notFound, success } from "@/utils/response";
 import { NextRequest } from "next/server";
 
-export /**
+/**
  * @description
  * @author 星月なるこ
  * @date 2025-01-22 22:54:21
  * @param {NextRequest} request
  * @return {success | notFound}
  */
-const GET = async (request: NextRequest) => {
+export const GET = async (request: NextRequest) => {
   /** @type {string | undefined} */
   const playerId: string | undefined = request.nextUrl.searchParams
     .get("playerId")
@@ -20,14 +20,14 @@ const GET = async (request: NextRequest) => {
   return success(player);
 };
 
-export /**
+/**
  * @description
  * @author 星月なるこ
  * @date 2025-01-22 22:55:00
  * @param {NextRequest} request
  * @return {*}
  */
-const POST = async (request: NextRequest) => {
+export const POST = async (request: NextRequest) => {
   const playerData = await request.json();
   const player = await createPlayer(playerData);
   if (!player) return internalServerError();
