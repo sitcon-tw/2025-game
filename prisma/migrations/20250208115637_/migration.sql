935166a8e0e638c56706c5c3a30d9137c2e1e6d0
星月なるこ <projectcsj@gmail.com>
2025-02-08 21:25:17 +0800

    feat: add database migration and update Prisma schema for player and team models

--- prisma/migrations/20250208115637_/migration.sql
+++ prisma/migrations/20250208115637_/migration.sql
@@ -1,23 +1,32 @@
 -- CreateTable
 CREATE TABLE "Player" (
-    "player_id" UUID NOT NULL,
+    "player_id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "avatar" TEXT,
-    "linktree" TEXT NOT NULL,
+    "linktree" TEXT,
     "stage" INTEGER NOT NULL DEFAULT 0,
     "currentStage" TEXT,
+    "team_id" UUID,
 
     CONSTRAINT "Player_pkey" PRIMARY KEY ("player_id")
+);
+
+-- CreateTable
+CREATE TABLE "PlayerBooth" (
+    "player_id" TEXT NOT NULL,
+    "booth_id" UUID NOT NULL,
+
+    CONSTRAINT "PlayerBooth_pkey" PRIMARY KEY ("player_id","booth_id")
 );
 
 -- CreateTable
 CREATE TABLE "Team" (
     "team_id" UUID NOT NULL,
     "name" TEXT NOT NULL,
-    "player1_id" UUID NOT NULL,
-    "player2_id" UUID NOT NULL,
-    "player3_id" UUID NOT NULL,
-    "player4_id" UUID NOT NULL,
+    "player1_id" TEXT NOT NULL,
+    "player2_id" TEXT NOT NULL,
+    "player3_id" TEXT NOT NULL,
+    "player4_id" TEXT NOT NULL,
 
     CONSTRAINT "Team_pkey" PRIMARY KEY ("team_id")
 );
@@ -32,16 +41,16 @@ CREATE TABLE "Fragment" (
 -- CreateTable
 CREATE TABLE "PlayerFragment" (
     "fragment_id" UUID NOT NULL,
-    "player_id" UUID NOT NULL,
+    "player_id" TEXT NOT NULL,
     "type" TEXT NOT NULL,
     "amount" INTEGER NOT NULL,
 
-    CONSTRAINT "PlayerFragment_pkey" PRIMARY KEY ("fragment_id")
+    CONSTRAINT "PlayerFragment_pkey" PRIMARY KEY ("fragment_id","player_id")
 );
 
 -- CreateTable
 CREATE TABLE "PlayerScoreboard" (
-    "player_id" UUID NOT NULL,
+    "player_id" TEXT NOT NULL,
     "score" INTEGER NOT NULL,
 
     CONSTRAINT "PlayerScoreboard_pkey" PRIMARY KEY ("player_id")
@@ -66,18 +75,15 @@ CREATE TABLE "Achievement" (
 -- CreateTable
 CREATE TABLE "AchievementStatus" (
     "achievement_id" UUID NOT NULL,
-    "player_id" UUID NOT NULL,
+    "player_id" TEXT NOT NULL,
 
-    CONSTRAINT "AchievementStatus_pkey" PRIMARY KEY ("achievement_id")
+    CONSTRAINT "AchievementStatus_pkey" PRIMARY KEY ("achievement_id","player_id")
 );
 
 -- CreateTable
 CREATE TABLE "Booth" (
     "booth_id" UUID NOT NULL,
     "name" TEXT NOT NULL,
-    "x" INTEGER NOT NULL,
-    "y" INTEGER NOT NULL,
-    "z" INTEGER NOT NULL,
 
     CONSTRAINT "Booth_pkey" PRIMARY KEY ("booth_id")
 );
@@ -120,3 +126,27 @@ CREATE UNIQUE INDEX "Team_player3_id_key" ON "Team"("player3_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Team_player4_id_key" ON "Team"("player4_id");
+
+-- AddForeignKey
+ALTER TABLE "Player" ADD CONSTRAINT "Player_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "Team"("team_id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlayerFragment" ADD CONSTRAINT "PlayerFragment_player_id_fkey" FOREIGN KEY ("player_id") REFERENCES "Player"("player_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlayerFragment" ADD CONSTRAINT "PlayerFragment_fragment_id_fkey" FOREIGN KEY ("fragment_id") REFERENCES "Fragment"("fragment_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlayerScoreboard" ADD CONSTRAINT "PlayerScoreboard_player_id_fkey" FOREIGN KEY ("player_id") REFERENCES "Player"("player_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TeamScoreboard" ADD CONSTRAINT "TeamScoreboard_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "Team"("team_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AchievementStatus" ADD CONSTRAINT "AchievementStatus_achievement_id_fkey" FOREIGN KEY ("achievement_id") REFERENCES "Achievement"("achievement_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AchievementStatus" ADD CONSTRAINT "AchievementStatus_player_id_fkey" FOREIGN KEY ("player_id") REFERENCES "Player"("player_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Blocker" ADD CONSTRAINT "Blocker_stage_id_fkey" FOREIGN KEY ("stage_id") REFERENCES "Stage"("stage_id") ON DELETE RESTRICT ON UPDATE CASCADE;
