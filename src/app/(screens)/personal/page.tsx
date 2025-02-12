f8a01f6f70e6aa12319f3e22c69f669611432ef2
HACO8888 <haco.tw@gmail.com>
2025-02-13 00:20:49 +0800

    feat: personal page about me bar

--- src/app/(screens)/personal/page.tsx
+++ src/app/(screens)/personal/page.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { SquareUserRound, UserRound } from "lucide-react";
+import { useQRCode } from 'next-qrcode';
+import { Button } from "@/components/ui/button";
 
 export default function PersonalPage() {
+  const { Canvas } = useQRCode();
   return (
-    <div className="flex h-full w-full flex-col px-[2rem] pt-[5rem]">
+    <div className="flex h-full w-full flex-col px-[2rem] pt-[2rem] text-[#4B5563]">
       <div className="h-[4rem] w-full bg-[#E5E7EB]">
         <div className="flex h-full w-full px-[0.75rem] py-[0.5rem]">
           <div className="flex h-full flex-initial">
@@ -22,11 +27,32 @@ export default function PersonalPage() {
           </div>
         </div>
       </div>
-      <div className="mt-[2rem] h-[20rem] w-full bg-[#E5E7EB]">
-        <div className="flex p-[1rem]">
+      <div className="mt-[2rem] h-auto w-full bg-[#E5E7EB]">
+        <div className="flex flex-col p-[1rem]">
           <div className="flex">
             <UserRound width={20} height={23} />
-            <p className="pl-[0.5rem]">About Me</p>
+            <p className="pl-[0.5rem] font-bold">關於我</p>
+          </div>
+          <div className="flex mx-auto">
+            <Canvas
+              text={'https://sitcon.org/2025'}
+              options={{
+                errorCorrectionLevel: 'M',
+                margin: 3,
+                scale: 4,
+                width: 200,
+                color: {
+                  dark: '#4B5563FF',
+                  light: '#E5E7EBFF',
+                },
+              }}
+            />
+          </div>
+          <div className="flex mx-auto">
+            <p>這是一段沒有意義的文字，這只是為了要充滿文字的頁面，讓這個頁面看起來很豐富，你覺得這個頁面怎麼樣呢？</p>
+          </div>
+          <div className="flex pt-[1rem]">
+            <Button className="mx-auto font-bold text-md bg-purple-500">分享</Button>
           </div>
         </div>
       </div>
