c26968855f5a13bf347b5caa58bb7fce105a0f98
HACO8888 <haco.tw@gmail.com>
2025-02-13 00:21:01 +0800

    feat: personal page about me bar

--- src/app/(screens)/personal/page.tsx
+++ src/app/(screens)/personal/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { SquareUserRound, UserRound } from "lucide-react";
-import { useQRCode } from 'next-qrcode';
+import { useQRCode } from "next-qrcode";
 import { Button } from "@/components/ui/button";
 
 export default function PersonalPage() {
@@ -33,26 +33,30 @@ export default function PersonalPage() {
             <UserRound width={20} height={23} />
             <p className="pl-[0.5rem] font-bold">關於我</p>
           </div>
-          <div className="flex mx-auto">
+          <div className="mx-auto flex">
             <Canvas
-              text={'https://sitcon.org/2025'}
+              text={"https://sitcon.org/2025"}
               options={{
-                errorCorrectionLevel: 'M',
+                errorCorrectionLevel: "M",
                 margin: 3,
                 scale: 4,
                 width: 200,
                 color: {
-                  dark: '#4B5563FF',
-                  light: '#E5E7EBFF',
+                  dark: "#4B5563FF",
+                  light: "#E5E7EBFF",
                 },
               }}
             />
           </div>
-          <div className="flex mx-auto">
-            <p>這是一段沒有意義的文字，這只是為了要充滿文字的頁面，讓這個頁面看起來很豐富，你覺得這個頁面怎麼樣呢？</p>
+          <div className="mx-auto flex">
+            <p>
+              這是一段沒有意義的文字，這只是為了要充滿文字的頁面，讓這個頁面看起來很豐富，你覺得這個頁面怎麼樣呢？
+            </p>
           </div>
           <div className="flex pt-[1rem]">
-            <Button className="mx-auto font-bold text-md bg-purple-500">分享</Button>
+            <Button className="text-md mx-auto bg-purple-500 font-bold">
+              分享
+            </Button>
           </div>
         </div>
       </div>
