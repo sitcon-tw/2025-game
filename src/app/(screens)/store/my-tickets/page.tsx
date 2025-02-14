0efd4819479f7a92327a9fcf3d9ecdf21402bb1b
Kevin Chang <kevin0216@kevin0216.us>
2025-02-14 12:42:23 +0800

    feat: added shop basic interface

---
+++ src/app/(screens)/store/my-tickets/page.tsx
@@ -0,0 +1,139 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { TicketPercent, Ticket } from "lucide-react";
+import { useState } from "react";
+
+export default function MyTicketsPage() {
+  const exampleLottery = [
+    {
+      name: "SITCON 2025 鍵帽",
+      amount: 2,
+      lottery_list: ["A00094"],
+    },
+    {
+      name: "獎品名稱 B",
+      amount: 10,
+      lottery_list: [],
+    },
+    {
+      name: "獎品名稱 C",
+      amount: 1,
+      lottery_list: [
+        "C00194",
+        "C00195",
+        "C00196",
+        "C00197",
+        "C00198",
+        "C00199",
+        "C00278",
+        "C00279",
+        "C00280",
+        "C00281",
+        "C00282",
+        "C00283",
+        "C00284",
+        "C00285",
+        "C00286",
+        "C00287",
+        "C00288",
+        "C00289",
+        "C00290",
+        "C00291",
+        "C00292",
+        "C00293",
+      ],
+    },
+  ];
+
+  const [expanded, setExpanded] = useState<Record<number, boolean>>({});
+
+  const toggleExpand = (index: number) => {
+    setExpanded((prev) => ({ ...prev, [index]: !prev[index] }));
+  };
+  return (
+    <>
+      <section id="coupons" className="w-full px-5 pt-14">
+        <p className="py-2 text-lg">紀念品折價卷</p>
+        <div className="flex flex-row items-center gap-3">
+          <div className="self-start">
+            <TicketPercent className="text-[#4b5c6bff]" size={50} />
+          </div>
+          <div className="flex flex-grow flex-col gap-2 pt-2">
+            {[{ amount: "5" }, { amount: "20" }, { amount: "50" }].map(
+              (coupon) => (
+                <div
+                  key={coupon.amount}
+                  className="flex items-center justify-between"
+                >
+                  <Button
+                    variant="default"
+                    className="flex-grow bg-[#6358ec] px-4 py-2 transition active:scale-95"
+                  >
+                    <span>{coupon.amount} 元折價卷</span>
+                  </Button>
+                </div>
+              ),
+            )}
+          </div>
+        </div>
+      </section>
+      <section id="lottery" className="w-full px-5 pt-2">
+        <p className="py-2 text-lg">抽獎卷</p>
+        <div className="flex flex-row items-center gap-3">
+          <div className="self-start">
+            <Ticket className="text-[#4b5c6bff]" size={50} />
+          </div>
+          <div className="flex flex-grow flex-col gap-2 pt-2">
+            {exampleLottery.map((lottery, index) => {
+              const isExpanded = expanded[index];
+              const displayedList = isExpanded
+                ? lottery.lottery_list
+                : lottery.lottery_list.slice(0, 20);
+
+              return (
+                <div
+                  key={index}
+                  className="flex flex-col items-start justify-between"
+                >
+                  <Button
+                    variant="outline"
+                    className="relative w-full cursor-default border-[#6358ec] px-4 py-2 text-[#6358ec] hover:border-[#6358ec] hover:bg-white hover:text-[#6358ec]"
+                  >
+                    <span>{lottery.name}</span>
+                    <span className="absolute bottom-0 right-0 pr-1 text-black">
+                      抽 {lottery.amount} 名
+                    </span>
+                  </Button>
+                  {lottery.lottery_list.length > 0 && (
+                    <>
+                      <div className="relative grid w-full grid-cols-[repeat(auto-fill,minmax(65px,1fr))]">
+                        {displayedList.map((lotteryId) => (
+                          <span
+                            key={lotteryId}
+                            className="whitespace-normal pl-2 text-black"
+                          >
+                            {lotteryId}
+                          </span>
+                        ))}
+                        {lottery.lottery_list.length > 20 && (
+                          <Button
+                            variant="ghost"
+                            className="absolute -bottom-8 right-0 h-fit w-fit text-black active:scale-90"
+                            onClick={() => toggleExpand(index)}
+                          >
+                            {isExpanded ? "收起" : "顯示更多"}
+                          </Button>
+                        )}
+                      </div>
+                    </>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/app/(screens)/store/page.tsx
+++ src/app/(screens)/store/page.tsx
@@ -1,7 +1,139 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  ArrowUpDown,
+  RotateCcwSquare,
+  Bomb,
+  SquareArrowOutUpRight,
+  TicketPercent,
+  Ticket,
+} from "lucide-react";
+
 export default function StorePage() {
+  const points = 12345678; // TODO: 積分獲取
+
   return (
     <>
-      <p>商店頁面</p>
+      <section id="points_overview" className="flex w-full">
+        <div className="ml-5 w-full py-3 pl-3">
+          <p className="text-lg text-gray-400">積分</p>
+          <p className="text-3xl">{points}</p>
+        </div>
+      </section>
+      <section id="props" className="w-full px-5">
+        <p className="pb-2 text-lg">道具</p>
+        <div className="grid grid-cols-4 gap-4">
+          <div className="flex flex-col items-center">
+            <div className="mb-2 bg-[#7b8795] p-1">
+              <ArrowUpDown className="text-[#4f5c6a]" size={40} />
+            </div>
+            <Button
+              variant="default"
+              className="w-auto bg-[#6358ec] transition active:scale-95"
+            >
+              <span className="text-base">1000點</span>{" "}
+              {/** TODO: fetch price from API **/}
+            </Button>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="mb-2 bg-[#7b8795] p-1">
+              <RotateCcwSquare className="text-[#4f5c6a]" size={40} />
+            </div>
+            <Button
+              variant="default"
+              className="w-auto bg-[#6358ec] transition active:scale-95"
+            >
+              <span className="text-base">500點</span>{" "}
+              {/** TODO: fetch price from API **/}
+            </Button>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="mb-2 bg-[#7b8795] p-1">
+              <Bomb className="text-[#4f5c6a]" size={40} />
+            </div>
+            <Button
+              variant="default"
+              className="w-auto bg-[#6358ec] transition active:scale-95"
+            >
+              <span className="text-base">500點</span>{" "}
+              {/** TODO: fetch price from API **/}
+            </Button>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="mb-2 bg-[#7b8795] p-1">
+              <SquareArrowOutUpRight className="text-[#4f5c6a]" size={40} />
+            </div>
+            <Button
+              variant="default"
+              className="w-auto bg-[#6358ec] transition active:scale-95"
+            >
+              <span className="text-base">2000點</span>{" "}
+              {/** TODO: fetch price from API **/}
+            </Button>
+          </div>
+        </div>
+      </section>
+      <section id="coupons" className="w-full px-5 pt-2">
+        <p className="py-2 text-lg">紀念品折價卷</p>
+        <div className="flex flex-row items-center gap-3">
+          <div className="self-start">
+            <TicketPercent className="text-[#4b5c6bff]" size={50} />
+          </div>
+          <div className="flex flex-grow flex-col gap-2 pt-2">
+            {[
+              { amount: "5", points: "1000", remaining: "500", limit: "2" },
+              { amount: "20", points: "5000", remaining: "50", limit: "1" },
+              { amount: "50", points: "10000", remaining: "10", limit: "1" },
+            ].map((coupon, index) => (
+              <div
+                key={coupon.amount}
+                className="flex items-center justify-between"
+              >
+                <Button
+                  variant="default"
+                  className="flex-grow bg-[#6358ec] px-4 py-2 transition active:scale-95"
+                >
+                  <span>
+                    {coupon.amount} 元折價卷 {coupon.points}點
+                  </span>
+                </Button>
+                <span className="ml-4 text-center text-sm">
+                  剩餘 {coupon.remaining} 張<br />
+                  每人限購 {coupon.limit} 張
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section id="coupons" className="w-full px-5 pt-2">
+        <p className="py-2 text-lg">抽獎卷</p>
+        <div className="flex flex-row items-center gap-3">
+          <div className="self-start">
+            <Ticket className="text-[#4b5c6bff]" size={50} />
+          </div>
+          <div className="flex flex-grow flex-col gap-2 pt-2">
+            {[
+              { amount: "1", points: "1000" },
+              { amount: "6", points: "5000" },
+              { amount: "15", points: "10000" },
+            ].map((ticket, index) => (
+              <div
+                key={ticket.amount}
+                className="flex items-center justify-between"
+              >
+                <Button
+                  variant="default"
+                  className="flex-grow bg-[#6358ec] px-4 py-2 transition active:scale-95"
+                >
+                  <span>
+                    {ticket.amount} 張 - {ticket.points}點
+                  </span>
+                </Button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </>
   );
 }
