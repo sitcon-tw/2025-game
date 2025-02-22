c074ead46e47b2952581ed52e373e6e942ba3058
Dada878 <xx0932399@gmail.com>
2025-02-22 17:47:42 +0800

    fix: initialize player points and score on creation

--- src/app/api/player/auth/route.ts
+++ src/app/api/player/auth/route.ts
@@ -8,6 +8,11 @@ export const POST = async (request: NextRequest) => {
   const result = await fetch(`https://sitcon.opass.app/status?token=${token}`);
   if (result.status === 400) return forbidden("並非本次與會者");
   const resultJson = await result.json();
-  await createPlayer({ token: data.token, name: resultJson.user_id });
+  await createPlayer({
+    token: data.token,
+    name: resultJson.user_id,
+    point: 0,
+    score: 0,
+  });
   return success({ message: "Authorized" });
 };
