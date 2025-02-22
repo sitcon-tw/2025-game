ef2111a341fc0fd84b0c045157c8e88b4f022ff9
Dada878 <xx0932399@gmail.com>
2025-02-22 17:51:50 +0800

    feat: instal add init react-query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,25 +6,36 @@ import { Blocks, Gamepad, LucideIcon, Store, UserRound } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
+
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="zh-Hant-TW">
       <body className="flex h-screen flex-col overflow-hidden">
-        <div className="relative h-full w-full overflow-y-scroll">
-          {children}
-        </div>
-        <div className="w-full">
-          <footer className="flex border-t-2 border-gray-400 bg-gray-200 px-2 py-2 text-center text-sm text-gray-600">
-            <NavbarItem href="/game" name="遊戲" Icon={Gamepad} />
-            <NavbarItem href="/fragment" name="板塊" Icon={Blocks} />
-            <NavbarItem href="/store" name="商店" Icon={Store} />
-            <NavbarItem href="/personal" name="個人" Icon={UserRound} />
-          </footer>
-        </div>
+        <QueryClientProvider client={queryClient}>
+          <div className="relative h-full w-full overflow-y-scroll">
+            {children}
+          </div>
+          <div className="w-full">
+            <footer className="flex border-t-2 border-gray-400 bg-gray-200 px-2 py-2 text-center text-sm text-gray-600">
+              <NavbarItem href="/game" name="遊戲" Icon={Gamepad} />
+              <NavbarItem href="/fragment" name="板塊" Icon={Blocks} />
+              <NavbarItem href="/store" name="商店" Icon={Store} />
+              <NavbarItem href="/personal" name="個人" Icon={UserRound} />
+            </footer>
+          </div>
+        </QueryClientProvider>
       </body>
     </html>
   );
@@ -34,11 +45,7 @@ function NavbarItem({
   href,
   name,
   Icon,
-}: Readonly<{
-  href: string;
-  name: string;
-  Icon: LucideIcon;
-}>) {
+}: Readonly<{ href: string; name: string; Icon: LucideIcon }>) {
   const pathname = usePathname();
   const getIsActive = () => {
     if (href === "/" && pathname !== "/") return false;
