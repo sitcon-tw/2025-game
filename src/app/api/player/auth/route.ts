da524553930045c5062b06b06bea672e20db343f
Dada878 <xx0932399@gmail.com>
2025-02-24 06:03:46 +0800

    feat: update auth api endpoint to return player data

--- src/app/api/player/auth/route.ts
+++ src/app/api/player/auth/route.ts
@@ -1,4 +1,4 @@
-import { createPlayer } from "@/utils/query";
+import { createPlayer, getPlayer } from "@/utils/query";
 import { forbidden, success } from "@/utils/response";
 import { NextRequest } from "next/server";
 
@@ -7,12 +7,23 @@ export const POST = async (request: NextRequest) => {
   const token = data.token;
   const result = await fetch(`https://sitcon.opass.app/status?token=${token}`);
   if (result.status === 400) return forbidden("並非本次與會者");
-  const resultJson = await result.json();
-  await createPlayer({
-    token: data.token,
-    name: resultJson.user_id,
-    point: 0,
-    score: 0,
-  });
-  return success({ message: "Authorized" });
+  // NOTE: 目前 OPass API 怪怪的，所以要先處理一下，等 API 修好後可以拿掉
+  const resultText = (await result.text()).replaceAll('\\"', '"').slice(1, -2);
+  const resultJson = JSON.parse(resultText);
+  const player = await getPlayer(token);
+  if (!player) {
+    await createPlayer({
+      token: data.token,
+      name: resultJson.user_id,
+      point: 0,
+      score: 0,
+    });
+    return success({
+      token: data.token,
+      name: resultJson.user_id,
+      point: 0,
+      score: 0,
+    });
+  }
+  return success(player);
 };
