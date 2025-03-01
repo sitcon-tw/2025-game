e64079dcdd5cc4ebdbb60463a7aa03f2cb28a526
Dada878 <xx0932399@gmail.com>
2025-03-01 23:38:35 +0800

    feat: refactor getBoothToken and getPlayerPuzzle functions for improved readability

--- src/lib/getBoothToken.ts
+++ src/lib/getBoothToken.ts
@@ -4,12 +4,11 @@ export async function getBoothToken(
   code: string,
   mappingURL: string = MAPPING_URL,
 ): Promise<string | null> {
-  return fetch(`${mappingURL}?code=${code}`)
-    .then((res) => {
-      return res.status === 200
-        ? res.text()
-        : mappingURL !== MAPPING_URL_BAK
+  return fetch(`${mappingURL}?code=${code}`).then((res) => {
+    return res.status === 200
+      ? res.text()
+      : mappingURL !== MAPPING_URL_BAK
         ? getBoothToken(code, MAPPING_URL_BAK)
         : null;
-    });
+  });
 }
