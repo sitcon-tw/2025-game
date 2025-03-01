10a9633af375ac101ccfde889bb6accbc26d9234
Dada878 <xx0932399@gmail.com>
2025-03-02 06:57:15 +0800

    feat: wrap layout components with Suspense for improved loading handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { usePathname } from "next/navigation";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TokenProvider } from "@/hooks/useToken";
+import { Suspense } from "react";
 
 // Create a client
 const queryClient = new QueryClient();
@@ -18,21 +19,23 @@ export default function RootLayout({
   return (
     <html lang="zh-Hant-TW">
       <body className="flex h-screen flex-col overflow-hidden">
-        <TokenProvider>
-          <QueryClientProvider client={queryClient}>
-            <div className="relative h-full w-full overflow-y-scroll">
-              {children}
-            </div>
-            <div className="w-full">
-              <footer className="flex border-t border-gray-700 bg-gray-200 bg-primary px-2 py-2 text-center text-sm text-primary-foreground">
-                <NavbarItem href="/game" name="遊戲" Icon={Gamepad} />
-                <NavbarItem href="/fragment" name="板塊" Icon={Blocks} />
-                <NavbarItem href="/store" name="商店" Icon={Store} />
-                <NavbarItem href="/personal" name="個人" Icon={UserRound} />
-              </footer>
-            </div>
-          </QueryClientProvider>
-        </TokenProvider>
+        <Suspense>
+          <TokenProvider>
+            <QueryClientProvider client={queryClient}>
+              <div className="relative h-full w-full overflow-y-scroll">
+                {children}
+              </div>
+              <div className="w-full">
+                <footer className="flex border-t border-gray-700 bg-gray-200 bg-primary px-2 py-2 text-center text-sm text-primary-foreground">
+                  <NavbarItem href="/game" name="遊戲" Icon={Gamepad} />
+                  <NavbarItem href="/fragment" name="板塊" Icon={Blocks} />
+                  <NavbarItem href="/store" name="商店" Icon={Store} />
+                  <NavbarItem href="/personal" name="個人" Icon={UserRound} />
+                </footer>
+              </div>
+            </QueryClientProvider>
+          </TokenProvider>
+        </Suspense>
       </body>
     </html>
   );
