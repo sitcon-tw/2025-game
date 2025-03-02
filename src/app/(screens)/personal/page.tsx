20d39c7d28f834eea0626828f214b3c86ec1de7b
Dada878 <xx0932399@gmail.com>
2025-03-02 09:14:57 +0800

    feat: update personal page theme

--- src/app/(screens)/personal/page.tsx
+++ src/app/(screens)/personal/page.tsx
@@ -10,8 +10,8 @@ export default function PersonalPage() {
   const { playerData, isError, isLoading, error } = usePlayerData();
 
   return (
-    <div className="flex h-full w-full flex-col px-[2rem] pt-[2rem] text-[#4B5563]">
-      <div className="w-full bg-[#E5E7EB]">
+    <div className="flex h-full w-full flex-col px-[2rem] pt-[2rem] text-foreground">
+      <div className="w-full rounded-lg bg-primary">
         <div className="flex h-full w-full px-[0.75rem] py-[0.5rem]">
           <div className="flex h-full flex-initial">
             <SquareUserRound
@@ -20,25 +20,25 @@ export default function PersonalPage() {
               height={"2rem"}
             />
           </div>
-          <div className="flex flex-none flex-col pl-[0.75rem] text-left font-bold">
+          <div className="flex flex-none flex-col pl-[0.75rem] text-left">
             <div>玩家名稱：</div>
             <div>個人排名：</div>
             <div>團體排名：</div>
           </div>
-          <div className="flex-1 flex-col pl-[0.5rem] text-right font-bold">
+          <div className="flex-1 flex-col pl-[0.5rem] text-right">
             <div>{playerData?.name}</div>
             <div>1000/2000</div>
             <div>10/99</div>
           </div>
         </div>
       </div>
-      <div className="mt-[2rem] h-auto w-full bg-[#E5E7EB]">
-        <div className="flex flex-col p-[1rem]">
-          <div className="flex">
+      <div className="mt-[2rem] h-auto w-full rounded-lg bg-primary text-foreground">
+        <div className="flex flex-col gap-4 p-[1rem]">
+          <div className="flex items-center">
             <UserRound width={20} height={23} />
-            <p className="pl-[0.5rem] font-bold">關於我</p>
+            <p>關於我</p>
           </div>
-          <div className="mx-auto flex">
+          <div className="mx-auto flex overflow-hidden rounded-lg">
             <Canvas
               text={playerData?.token ?? "null"}
               options={{
@@ -58,11 +58,11 @@ export default function PersonalPage() {
               這是一段沒有意義的文字，這只是為了要充滿文字的頁面，讓這個頁面看起來很豐富，你覺得這個頁面怎麼樣呢？
             </p>
           </div>
-          <div className="flex pt-[1rem]">
+          {/* <div className="flex pt-[1rem]">
             <Button className="text-md mx-auto bg-purple-500 font-bold">
               分享
             </Button>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
