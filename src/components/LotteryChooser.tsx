572f98533009e6afe005745925234b5a116c681f
Kevin Chang <kevin0216@kevin0216.us>
2025-03-04 21:01:52 +0800

    feat: rebuild LotteryChooser with API connected

--- src/components/LotteryChooser.tsx
+++ src/components/LotteryChooser.tsx
@@ -4,30 +4,30 @@ import { AnimatePresence, motion } from "framer-motion";
 import { Archive, ChevronUp, ChevronDown, X, OctagonAlert } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useState, useEffect } from "react";
+import { Lottery, LotteryItem } from "@/lib/interface";
 
-function LotteryItem({
+function LotteryListing({
   lottery,
   amounts,
   lotteryRecord,
+  myLottery,
   lotteryIndex,
   toggleLotteryUp,
   toggleLotteryDown,
 }: {
-  lottery: {
-    id: number;
-    name: string;
-    amount: number;
-    lottery_list: string[];
-  };
+  lottery: LotteryItem;
   amounts: number;
   lotteryRecord: Record<number, number>;
+  myLottery: Lottery | undefined;
   lotteryIndex: number;
   toggleLotteryUp: (index: number) => void;
   toggleLotteryDown: (index: number) => void;
 }) {
+  const submitted = myLottery?.lottery_list.length ?? 0;
+
   return (
     <div className="flex w-full flex-row items-center">
-      <Archive size={80} className="mr-2 text-primary" />
+      <Archive size={80} className="mr-2 text-white" />
       <div className="flex flex-col items-center">
         <Button
           variant="ghost"
@@ -52,9 +52,9 @@ function LotteryItem({
       <span className="flex-grow break-words pl-1 text-[24px]">
         {lottery.name}
         <div>
-          <span className="text-base">抽 {lottery.amount} 名 | </span>
+          <span className="text-base">抽 {lottery.maxDrawn} 名 | </span>
           <span className="text-base">
-            {lotteryRecord[lotteryIndex]} / {lottery.lottery_list.length}
+            {lotteryRecord[lotteryIndex] + submitted} / {lottery.total}
           </span>
         </div>
       </span>
@@ -64,60 +64,22 @@ function LotteryItem({
 
 export default function LotteryChooser({
   amount,
+  lotteryList,
+  myLotteryList,
+  token,
   isOpen,
-  setIsOpen,
+  setIsOpenAction,
 }: {
   amount: number;
+  lotteryList: LotteryItem[];
+  myLotteryList: Lottery[];
+  token: string;
   isOpen: boolean;
-  setIsOpen: (isOpen: boolean) => void;
+  setIsOpenAction: (isOpen: boolean) => void;
 }) {
-  const exampleLottery = [
-    {
-      id: 0,
-      name: "SITCON 2025 鍵帽",
-      amount: 2,
-      lottery_list: ["A00094"],
-    },
-    {
-      id: 1,
-      name: "獎品名稱 B",
-      amount: 10,
-      lottery_list: [],
-    },
-    {
-      id: 2,
-      name: "獎品名稱 C",
-      amount: 1,
-      lottery_list: [
-        "C00194",
-        "C00195",
-        "C00196",
-        "C00197",
-        "C00198",
-        "C00199",
-        "C00278",
-        "C00279",
-        "C00280",
-        "C00281",
-        "C00282",
-        "C00283",
-        "C00284",
-        "C00285",
-        "C00286",
-        "C00287",
-        "C00288",
-        "C00289",
-        "C00290",
-        "C00291",
-        "C00292",
-        "C00293",
-      ],
-    },
-  ];
-
   const [amounts, setAmounts] = useState(0);
   const [lottery, setLottery] = useState<Record<number, number>>(
-    exampleLottery.map(() => 0),
+    lotteryList.map(() => 0),
   );
 
   const toggleLotteryUp = (index: number) => {
@@ -133,14 +95,24 @@ export default function LotteryChooser({
     setAmounts((prev) => prev + 1);
   };
 
-  const submitLottery = () => {
-    console.log(lottery);
-    setLottery(exampleLottery.map(() => 0));
-    setIsOpen(false);
+  const submitLottery = async () => {
+    const submission = lotteryList.map((lotteryItem, index) => {
+      return { id: lotteryItem.id, num: lottery[index] };
+    });
+    // console.log(JSON.stringify(submission));
+    const result = await fetch(`/api/lottery/submit?token=${token}`, {
+      method: "POST",
+      body: JSON.stringify(submission),
+    });
+    if (result.ok) {
+      setIsOpenAction(false);
+      setLottery(lotteryList.map(() => 0));
+      window.location.reload();
+    }
   };
   const closeDialog = () => {
-    setLottery(exampleLottery.map(() => 0));
-    setIsOpen(false);
+    setLottery(lotteryList.map(() => 0));
+    setIsOpenAction(false);
   };
 
   useEffect(() => {
@@ -160,7 +132,7 @@ export default function LotteryChooser({
           style={{ pointerEvents: "auto" }}
           className="fixed inset-0 z-50 m-0 flex items-center justify-center bg-black/60 text-foreground"
           onClick={() => {
-            setIsOpen(false);
+            setIsOpenAction(false);
           }}
         >
           <motion.div
@@ -171,8 +143,8 @@ export default function LotteryChooser({
             onClick={(e) => e.stopPropagation()}
             className="flex h-full w-full flex-col items-center rounded-lg bg-background p-8 md:static md:max-w-[70%] md:justify-start lg:max-w-[60%]"
           >
-            <div className="mb-5 flex w-full flex-row items-center text-start text-3xl font-bold text-primary">
-              <Archive size={48} className="mr-2 text-primary" />
+            <div className="mb-5 flex w-full flex-row items-center text-start text-3xl font-bold text-white">
+              <Archive size={48} className="mr-2 text-white" />
               <span>抽獎箱</span>
               <Button
                 variant="ghost"
@@ -183,18 +155,23 @@ export default function LotteryChooser({
               </Button>
             </div>
             <div
-              className={`flex w-full pl-1 ${amounts == 0 ? "text-red-500" : ""}`}
+              className={`flex w-full pl-1 ${amounts == 0 ? "text-red-400" : ""}`}
             >
               <p className="pr-2 text-left text-2xl font-bold">{amounts}</p>
               <p className="self-end text-left"> / {amount} 張抽獎卷可用</p>
             </div>
             <hr className="my-6 h-0.5 w-full border-t-0 bg-gray-400" />
             <div className="flex w-full flex-col gap-4">
-              {exampleLottery.map((lotteryItem, index) => (
-                <LotteryItem
+              {lotteryList.map((lotteryItem, index) => (
+                <LotteryListing
                   key={index}
                   amounts={amounts}
                   lottery={lotteryItem}
+                  myLottery={
+                    myLotteryList.filter(
+                      (value) => value.type == lotteryItem.id,
+                    )[0]
+                  }
                   lotteryRecord={lottery}
                   lotteryIndex={index}
                   toggleLotteryUp={toggleLotteryUp}
@@ -204,7 +181,7 @@ export default function LotteryChooser({
             </div>
             <div className="absolute bottom-3 w-full px-10">
               {amounts != 0 && (
-                <span className="mt-2 flex flex-row items-center justify-center gap-2 text-red-500">
+                <span className="mt-2 flex flex-row items-center justify-center gap-2 text-red-400">
                   <OctagonAlert size={24} />
                   您還有抽獎卷沒使用！
                 </span>
