2d02fb9a9e23f3057bd5f0deea560b68eb8b9f6d
Kevin S.H. Chang <52964758+kevin0216@users.noreply.github.com>
2025-03-08 12:23:28 +0800

    feat: add possibility

--- src/components/LotteryChooser.tsx
+++ src/components/LotteryChooser.tsx
@@ -55,7 +55,7 @@ function LotteryListing({
         <div>
           <span className="text-base">抽 {lottery.maxDrawn} 名 | </span>
           <span className="text-base">
-            {lotteryRecord[lotteryIndex] + submitted} / {lottery.total}
+            {lotteryRecord[lotteryIndex] + submitted} / {lottery.total} ({((lotteryRecord[lotteryIndex] + submitted) / lottery.total).toFixed(2)}%)
           </span>
         </div>
       </span>
